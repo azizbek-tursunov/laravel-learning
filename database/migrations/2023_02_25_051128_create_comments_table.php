7abf12f42f12e8fd5769571c3691c26154c74ded
azizbek-tursunov <azizbektursunovofficial@gmail.com>
2023-02-26 19:35:48 +0500

    Commits

--- database/migrations/2023_02_25_051128_create_comments_table.php
+++ database/migrations/2023_02_25_051128_create_comments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->text('body');
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
@@ -22,4 +23,8 @@ Route::get('/service', [PageController::class, 'service'])->name('service');
 Route::get('/project', [PageController::class, 'project'])->name('project');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
 
-Route::resource('/posts', PostController::class);
+Route::resources([
+    'posts' => PostController::class,
+    'comments' => CommentController::class,
+    'users' => UserController::class
+]);
